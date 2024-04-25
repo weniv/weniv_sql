b007d1a78cd50412e6be2ca2344df524438c6fb8
baekg <baekg.dev@gmail.com>
2024-04-25 16:37:07 +0900

    :memo: Edit: 클릭 이벤트 수집

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -1,6 +1,6 @@
 const BASE_URL = 'https://www.analytics.weniv.co.kr';
 
-function sendPageViewData() {
+function collectPageView() {
   fetch(`${BASE_URL}/collect/pageview`, {
     method: 'POST',
     headers: {
@@ -16,9 +16,38 @@ function sendPageViewData() {
     })
     .then((data) => {
       sessionStorage.setItem('session_id', data.session_id);
-      console.log(data);
     })
     .catch((error) => console.error('Error:', error));
 }
+window.addEventListener('load', collectPageView);
 
-window.addEventListener('load', sendPageViewData);
+function collectAnchorClick(event) {
+  console.log(event);
+  const session_id = sessionStorage.getItem('session_id');
+
+  const source_url = window.location.href;
+  const target_url = event.target.href;
+
+  fetch(`${BASE_URL}/collect/anchor-click`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Session-Id': session_id,
+    },
+    body: JSON.stringify({ source_url, target_url }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then((data) => console.log(data))
+    .catch((error) => console.error('Error:', error));
+}
+
+window.addEventListener('click', (event) => {
+  if (event.target.tagName === 'A') {
+    collectAnchorClick(event);
+  }
+});
